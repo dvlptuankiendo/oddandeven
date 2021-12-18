268380ec08797f3138f23469a6444d1596f8b40c
= <=>
2021-12-18 10:06:29 +0700

    feat(chat): chat fe logic

--- client/src/components/ChatCard.js
+++ client/src/components/ChatCard.js
@@ -1,15 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Button, Input, Form } from "reactstrap";
+import { FaUserAlt } from "react-icons/fa";
 
 import ContentCard from "./ContentCard";
+import firebase from "../configs/firebase";
+import { AppContext } from "../contexts/app.context";
+
+const messageStore = firebase.firestore().collection("Messages");
 
 const ChatCard = () => {
+  const { user } = useContext(AppContext);
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
+  const [messageId, setMessageId] = useState(null);
+  const [newMessage, setNewMessage] = useState(null);
+
+  useEffect(() => {
+    messageStore.onSnapshot((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      if (data[0]) {
+        !messageId && setMessageId(data[0].id);
+        setNewMessage(data[0]);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (newMessage) {
+      setMessages([...messages, newMessage]);
+      setNewMessage(null);
+    }
+  }, [newMessage]);
+
+  useEffect(() => {
+    const bottom = document.getElementById("message-list-bottom");
+    bottom && bottom.scrollIntoView();
+  }, [messages]);
+
+  const send = async (e) => {
+    e.preventDefault();
+    if (!message || !message.trim()) return;
+    if (!messageId) {
+      const allMessages = await messageStore.get();
+      const docIds = allMessages.docs.map((doc) => doc.id);
+      docIds[0] && setMessageId(docIds[0]);
+      await messageStore.doc(docIds[0]).update({
+        sender: (user && user.username) || "Vô danh",
+        text: message,
+        amount: (user && user.amount) || "",
+      });
+      return;
+    }
+
+    await messageStore.doc(messageId).update({
+      sender: user.username,
+      text: message,
+      amount: user.amount,
+    });
+
+    setMessage("");
+  };
+
+  const renderAmount = (item) => {
+    if (!item.amount) return "";
+    return `- Số dư: ${`${item.amount}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+  };
+
   return (
     <ContentCard title="BOX CHAT">
       <div className="d-flex flex-column box-chat">
-        <div className="flex-grow-1 mb-2 message-list"></div>
-        <Form>
+        <div className="flex-grow-1 mb-2 message-list">
+          {messages.map((item, index) => (
+            <div
+              key={index}
+              className="d-flex align-items-center"
+              style={{
+                justifyContent:
+                  item.sender === user.username ? "flex-end" : "flex-start",
+              }}
+            >
+              <div className="mb-2 message-item">
+                <div className="d-flex align-items-center mb-2">
+                  <FaUserAlt className="mr-2" />
+                  {item.sender} {renderAmount(item)}
+                </div>
+                <div>{item.text}</div>
+              </div>
+            </div>
+          ))}
+          <div id="message-list-bottom" />
+        </div>
+        <Form onSubmit={send}>
           <Input
             className="mb-2"
             placeholder="Nhập nội dung chat"
